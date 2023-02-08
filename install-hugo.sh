bdd4b57dac96b4baf19e0c21547146936320e97a
Tim Wilson <tim@timwilson.info>
2023-02-07 20:32:34 -0600

    Add theme option.

--- install-hugo.sh
+++ install-hugo.sh
@@ -3,7 +3,10 @@
 # Install Hugo in the folder and create a 'www' directory to store the 
 # markdown and config files.
 
+THEME_URL="${1:-https://themes.gohugo.io/themes/hugo-theme-charlolamode/}
+THEME_NAME=charlolamode
+
 hugo new site www
 cd www
-git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke themes/ananke
-echo "theme = 'ananke'" >> config.toml
+git submodule add $THEME_URL themes/$THEME_NAME
+echo "theme = '$THEME_NAME'" >> config.toml
