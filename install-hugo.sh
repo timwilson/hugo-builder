911c1563b39581e042e3ec5dba719f7bf6998e01
Tim Wilson <tim@timwilson.info>
2023-02-07 20:50:58 -0600

    Fix THEME_URL variable assignment.

--- install-hugo.sh
+++ install-hugo.sh
@@ -3,7 +3,7 @@
 # Install Hugo in the folder and create a 'www' directory to store the 
 # markdown and config files.
 
-THEME_URL="${1:-https://themes.gohugo.io/themes/hugo-theme-charlolamode/}"
+THEME_URL="${1:-'https://themes.gohugo.io/themes/hugo-theme-charlolamode/'}"
 THEME_NAME=charlolamode
 
 hugo new site www
